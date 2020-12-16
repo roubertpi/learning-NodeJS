67fc09a376ea1954d537b12b10ea37282327174a
roubertpi <roubertpimentar@gmail.com>
2020-12-16 00:58:24 -0300

    create a routing

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const fs = require ('fs');
 
 const http= require ('http');
+const url= require ('url');
+
+
 
 ///
 //server
 
 const server = http.createServer((req,res)=>{
-    res.end('Hello the first serve in Node Js')
+    const pathName= req.url;
+    if (pathName==='/' ||pathName ==='/overview'){
+        res.end('This the OverView');
+    }else if (pathName === '/protuct'){
+        res.end('This the Product')
+    }else{
+        res.writeHead(404,{
+            'Content-type':'text/html',
+            'my-own-header': 'hello-word'
+        });
+      res.end('<h1>Page not Foud');
+    }
 });
 server.listen(8000,()=>{
     console.log('listenig to requests on port8000');
