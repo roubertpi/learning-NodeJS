46b722c21c89308bbb0a84b37b888c6a00031683
roubertpi <roubertpimentar@gmail.com>
2020-12-17 01:01:28 -0300

    Html Templation 14 - forma dinamica usando apapenas JSON

--- index.js
+++ index.js
@@ -3,18 +3,54 @@ const fs = require ('fs');
 const http= require ('http');
 const url= require ('url');
 
+const replaceTemplate = (temp,product)=>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output= output.replace(/{%IMAGE%}/g, product.image);
+    output= output.replace(/{%PRICE%}/g, product.price);
+    output= output.replace(/{%FROM%}/g, product.from);
+    output= output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output= output.replace(/{%QUANTITY%}/g, product.quantity);
+    output= output.replace(/{%DESCRIPTION%}/g, product.description);
+    output= output.replace(/{%ID%}/g, product.id);
 
+    if(!product.organic) 
+    output=output.replace(/{%NOT_ORGANIC%}/g,'not_organic')
+        return output;
+    
+}
 
 ///
 //server
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data); 
+
 
 const server = http.createServer((req,res)=>{
     const pathName= req.url;
+
+    //Overview page
     if (pathName==='/' ||pathName ==='/overview'){
-        res.end('This the OverView');
+        res.writeHead(200,{ 'Content-type':'text/html'});
+
+       const cardsHtml =
+        dataObj.map(el=> replaceTemplate(tempCard,el)).join('');
+        const output=tempOverview.replace('{%PRODUCT_CARD%}',cardsHtml);
+        res.end(output);
+    
+    //Product page
     }else if (pathName === '/protuct'){
         res.end('This the Product')
-    }else{
+    // API
+    }else if (pathName==='/api'){
+       res.writeHead(200,{ 'Content-type':'application/json'});
+       res.end(data);
+    }
+    // Not Foud
+    else{
         res.writeHead(404,{
             'Content-type':'text/html',
             'my-own-header': 'hello-word'
@@ -22,6 +58,6 @@ const server = http.createServer((req,res)=>{
       res.end('<h1>Page not Foud');
     }
 });
-server.listen(8000,()=>{
+server.listen(8000,'127.0.0.1',()=>{
     console.log('listenig to requests on port8000');
 })
